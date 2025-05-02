70a5297c6e48841eae70a71c845291d92f502ff6
TruptiCoder <truptibalbudhe05@gmail.com>
2025-05-02 15:04:22 +0530

    Authentication

--- Video-Conferencing-App/Backend/src/app.js
+++ Video-Conferencing-App/Backend/src/app.js
@@ -9,6 +9,7 @@ import mongoose from "mongoose";
 import { connectToSocket } from "./controllers/socketManager.js";
 
 import cors from "cors";
+import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 const server = createServer(app);
@@ -19,6 +20,8 @@ app.set("port", (process.env.PORT || 8000));
 app.use(cors());
 app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb", extended: true}));
+
+app.use("/api/v1/users", userRoutes);
 
 const start = async() => {
     const connectionDb = await mongoose.connect(process.env.MONGO_URL);
